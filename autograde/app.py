867e0e6dc6087361a6906c79ff02ec34e8ec4c9d
Re'em Kishinevsky <Re'em Kishinevsky>
2025-09-24 18:47:15 +0300

    update app

--- autograde/app.py
+++ autograde/app.py
@@ -1,48 +1,130 @@
+import json
+import logging
+import os
 from pathlib import Path
 from typing import Annotated, Literal, Sequence
 
 import cyclopts
-from cyclopts import Parameter
+from cyclopts import CycloptsPanel, Parameter
+from cyclopts.types import ExistingFile, NonExistentDirectory
+from rich.console import Console
+from rich.logging import RichHandler
 
-from autograde.grader import Grader
+from autograde.grader import Assignment, GradingEnvironment
+from autograde.importers import Importer
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(message)s",
+    handlers=[RichHandler()]
+)
 
 
 class App:
     def __init__(
-        self, graders: Sequence[Grader], name="autograde", help="Automated grading tool"
+        self,
+        assignments: Sequence[Assignment],
+        importers: Sequence[Importer],
+        name: str = "autograde",
+        help: str = "Automated grading tool",
     ):
-        self.graders = graders
-        self._app = cyclopts.App(name=name, help=help)
+        self.assignments: list[Assignment] = list(assignments)
+        self.importers: list[Importer] = list(importers)
+        self.genv: GradingEnvironment | None = GradingEnvironment.from_path(Path.cwd())
+
+        self._app: cyclopts.App = cyclopts.App(name=name, help=help)
+        self._console = Console()
+
+
         self._app.command()(self.init())
+        self._app.command()(self.import_)
         self._app.command()(self.test)
         self._app.command()(self.grade)
         self._app.command()(self.reset)
 
+    def command(self, func, *args, **kwargs):
+        return self._app.command(*args, **kwargs)(func)
+
     def init(self):
-        def _init(ex: str, zipfile: Path, csvfile: Path, envdir: Path):
-            """Initialize testing environment
+        def _init(
+            assignment: str,
+            envdir: NonExistentDirectory,
+        ):
+            """Initialize grading environment.
 
             Parameters
             ----------
-            ex : str
+            assignment : str
                 Exercise name
-            zipfile : Path
-                Submissions zipfile
-            csvfile : Path
-                Grading worksheet csvfile
             envdir : Path
                 Grading environment directory (must not exist)
             """
-            print(
-                f"Initializing with exercise: {ex}, zipfile: {zipfile}, csvfile: {csvfile}, envdir: {envdir}"
-            )
-        _init.__annotations__["ex"] = Literal[self.graders[0].__class__.__name__]
+            dotdir = envdir / ".autograde"
+            os.makedirs(dotdir, exist_ok=False)
+            with open(dotdir / "autograde.json", "w") as f:
+                json.dump({"assignment_id": assignment}, f, indent=4)
+            logging.info(f"Created grading environment for assignment `{assignment}` at {envdir.absolute()}")
+
+            # set self.genv to a new GradingEnvironment
+            # self._genv = GradingEnvironment(envdir, next(a for a in self.assignments if a.id == assignment))
+            # self._genv.setup()
+
+        _init.__annotations__["assignment"] = Literal[*(a.id for a in self.assignments)]
         return _init
 
-    def test(self,
-             expression: Annotated[str | None, Parameter(name=("expression", "-k"))] = None,
-             pytest_args: list[str] = []):
-        """Test submissions
+    # def init(self):
+    #     def _init(
+    #         ex: str,
+    #         zipfile: ExistingFile,
+    #         csvfile: ExistingCsvPath,
+    #         envdir: NonExistentDirectory,
+    #     ):
+    #         """Initialize testing environment
+    #
+    #         Parameters
+    #         ----------
+    #         ex : str
+    #             Exercise name
+    #         zipfile : Path
+    #             Submissions zipfile
+    #         csvfile : Path
+    #             Grading worksheet csvfile
+    #         envdir : Path
+    #             Grading environment directory (must not exist)
+    #         """
+    #         print(
+    #             f"Initializing with exercise: {ex}, zipfile: {zipfile}, csvfile: {csvfile}, envdir: {envdir}"
+    #         )
+    #
+    #     _init.__annotations__["ex"] = Literal[*(a.id for a in self.assignments)]
+    #     return _init
+
+    def import_(
+        self,
+        file: ExistingFile,
+    ):
+        """Import file into grading environment.
+
+        Parameters
+        ----------
+        file : ExistingFile
+            File to import (e.g., submissions zipfile)
+        """
+        if self.genv is None:
+            self.error("Not in a grading environment. Please run 'init' first.")
+        for importer in self.importers:
+            if importer.should_handle(file):
+                importer.handle(file, self.genv)
+                break
+        else:
+            self.error(f"No importer found for file: {file}")
+
+    def test(
+        self,
+        expression: Annotated[str | None, Parameter(name=("expression", "-k"))] = None,
+        pytest_args: list[str] = [],
+    ):
+        """Test submissions.
 
         Parameters
         ----------
@@ -55,16 +137,19 @@ class App:
         pass
 
     # @app.command(help="Grade tested submissions")
-    def grade():
-        """Grade tested submissions"""
+    def grade(self):
+        """Grade tested submissions."""
         print("Grading submissions...")
-        # handle_file_not_found_error(cmd.grade)
 
     # @app.command(help="Reset grading environment")
-    def reset():
-        """Reset grading environment"""
+    def reset(self):
+        """Reset grading environment."""
         print("Resetting grading environment...")
         # handle_file_not_found_error(cmd.reset)
+
+    def error(self, msg: str):
+        Console().print(CycloptsPanel(msg))
+        quit(1)
 
     def __call__(self, *args, **kwargs):
         return self._app(*args, **kwargs)
